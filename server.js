2a24c89254df8b6fdef7460c1db5ec1ee9970b60
aryashah5000 <aryashah5000@gmail.com>
2025-05-27 19:13:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
 const app = express();
 app.get('/', (req,res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('Server on http://localhost:3000'));
+const port = process.env.port || 3000;
+app.listen(port, () => console.log('Server on ${port}'));
